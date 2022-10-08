a24dfa3f35153f6e255500f8f5779d8ffb5774c7
PraveenChordia <praveenchordia111.pc@gmail.com>
2022-10-08 12:17:00 +0530

    Commiting output files as example

--- StockHistory.py
+++ StockHistory.py
@@ -1,9 +1,5 @@
-import logging
 import pandas as pd
 import yfinance as yf
-
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
 
 '''
 In this program we will extract the data for last 90 days on which NSE was active.
@@ -15,14 +11,16 @@ i.e. The final CSV file will contain last 90 closing price for all stocks except
 # Getting Stock Symbols from the CSV file.
 with open("List of Stocks and their Symbols.csv", 'r') as stock_list:
     data = pd.read_csv(stock_list)
+
+# We add '.NS' at end of each stock to get Data of NSE, alternatively we can add '.BSE' to get data from BSE.
 symbols = [(x[1].strip() + '.NS') for x in data.values]
 
 '''
 We download data from 1st May, 2022 and take the last 90 entries.
 so we get the last 90 entries
 '''
-data = yf.download(symbols, '2022-05-01', progress=False)['Adj Close'][-90:]
-failed_symbols = pd.DataFrame({'Failed': list(yf.shared._ERRORS.keys())})
+data = yf.download(symbols, '2022-05-01')['Adj Close'][-90:]
+failed_symbols = pd.DataFrame({'Failed': list(yf.multi.shared._ERRORS.keys())})
 
 # Saving the data to CSV file.
 data.to_csv('last_90_days_record.csv')
